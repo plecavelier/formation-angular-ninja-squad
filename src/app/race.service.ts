80458ca2c26c4dbb837e4e989c6d66aa41ad87a8
plecavelier <pierre.lecavelier@gmail.com>
2017-04-05 11:54:24 +0200

    Exercice 4

--- src/app/race.service.ts
+++ src/app/race.service.ts
@@ -8,6 +8,12 @@ export class RaceService {
   constructor() { }
 
   list(): RaceModel[] {
-    return [{name: 'Lyon'}, {name: 'London'}];
+    return [
+      {name: 'Lyon'},
+      {name: 'Los Angeles'},
+      {name: 'Sydney'},
+      {name: 'Tokyo'},
+      {name: 'Casablanca'}
+    ];
   }
 }
