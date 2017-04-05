9995a8063d745206e160f6296208e848422cbe71
plecavelier <pierre.lecavelier@gmail.com>
2017-04-05 21:23:41 +0200

    Exercice 10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RacesComponent } from './races/races.component';
-
 import { RaceService } from './race.service';
 import { RaceComponent } from './race/race.component';
 import { PonyComponent } from './pony/pony.component';
 import { FromNowPipe } from './from-now.pipe';
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -19,12 +20,14 @@ import { FromNowPipe } from './from-now.pipe';
     RacesComponent,
     RaceComponent,
     PonyComponent,
-    FromNowPipe
+    FromNowPipe,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [
     RaceService
