4108cd8136685e3a6fb88c2ddad2caa8404e3d61
plecavelier <pierre.lecavelier@gmail.com>
2017-04-05 16:10:22 +0200

    Exercices 7,8

---
+++ src/app/pony/pony.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+import { PonyModel } from '../models/pony.model';
+
+@Component({
+  selector: 'pr-pony',
+  templateUrl: './pony.component.html',
+  styleUrls: ['./pony.component.css']
+})
+export class PonyComponent implements OnInit {
+
+  @Input() ponyModel: PonyModel;
+  @Output() ponyClicked: EventEmitter<PonyModel> = new EventEmitter<PonyModel>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getPonyImageUrl() {
+    return `assets/images/pony-${this.ponyModel.color.toLowerCase()}.gif`;
+  }
+
+  clicked() {
+    console.log('Click Pony');
+    this.ponyClicked.emit(this.ponyModel);
+  }
+}
